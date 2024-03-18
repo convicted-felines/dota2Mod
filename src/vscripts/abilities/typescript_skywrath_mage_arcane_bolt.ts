d2e179f78df5da66a92cf9865b2b38501d6ac6c2
convicted-felines <13347082172@189.cn>
2024-03-18 12:30:01 +0800

    天怒法球技能

--- src/vscripts/abilities/typescript_skywrath_mage_arcane_bolt.ts
+++ src/vscripts/abilities/typescript_skywrath_mage_arcane_bolt.ts
@@ -1,15 +1,56 @@
 import { BaseAbility, registerAbility } from "../lib/dota_ts_adapter";
 
 @registerAbility()
-export class typescript_skywrath_mage_arcane_bolt extends BaseAbility{
-    sound_cast :string ="Hero_SkywrathMage.ArcaneBolt.Cast";
-    sound_impact:string ="Hero_SkywrathMage.ArcaneBolt.Impact";
+export class typescript_skywrath_mage_arcane_bolt extends BaseAbility {
+    sound_cast: string = "Hero_SkywrathMage.ArcaneBolt.Cast";
+    sound_impact: string = "Hero_SkywrathMage.ArcaneBolt.Impact";
     projectile_arcane_bolt: string = "particles/units/heroes/hero_skywrath_mage/skywrath_mage_arcane_bolt.vpcf";
 
     OnSpellStart(): void {
-        const target =this.GetCursorTarget();
+        const target = this.GetCursorTarget();
+        const bolt_speed = this.GetSpecialValueFor("bolt_speed");
+        const bolt_vision = this.GetSpecialValueFor("bolt_vision");
 
+        ProjectileManager.CreateTrackingProjectile({
+            Ability: this,
+            EffectName: this.projectile_arcane_bolt,
+            Source: this.GetCaster(),
+            Target: target,
+            bDodgeable: false,
+            bProvidesVision: true,
+            iMoveSpeed: bolt_speed,
+            iVisionRadius: bolt_vision,
+            iVisionTeamNumber: this.GetCaster().GetTeamNumber()
+        })
+
+    }
+
+    OnProjectileHit(target: CDOTA_BaseNPC | undefined, location: Vector): boolean | void {
+        if (!target) { return; }
+
+        EmitSoundOn(this.sound_impact,target);
         
+        const bolt_vision=this.GetSpecialValueFor("bolt_vision");
+        const bolt_damage=this.GetSpecialValueFor("bolt_damage");
+        const int_multiplier=this.GetSpecialValueFor("int_multiplier");
+        const vision_duration=this.GetSpecialValueFor("vision_duration");
+
+        AddFOWViewer(this.GetCaster().GetTeamNumber(),location,bolt_vision,vision_duration,false);
+
+        let damage=bolt_damage;
+
+        if(this.GetCaster().IsHero()){
+            damage+=(this.GetCaster() as CDOTA_BaseNPC_Hero).GetIntellect()*int_multiplier;
+        }
+
+        ApplyDamage({
+            attacker:this.GetCaster(),
+            damage:damage,
+            damage_type:DamageTypes.MAGICAL,
+            victim:target,
+            ability:this,
+            damage_flags:DamageFlag.NONE
+        })
     }
 
 }
